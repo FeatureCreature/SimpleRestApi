e31609726121266e94a7b518dfd43307ddb277c0
FeatureCreature <featurecreature@protonmail.com>
2021-09-18 12:30:35 -0500

    Added controller endpoint to return a string.

--- src/main/kotlin/com/demo/simplerestapi/AppRestController.kt
+++ src/main/kotlin/com/demo/simplerestapi/AppRestController.kt
@@ -1,10 +1,14 @@
 package com.demo.simplerestapi
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AppRestController {
 
-
+    @GetMapping("/go")
+    fun go(): String{
+        return "Gone!!!"
+    }
 
 }
